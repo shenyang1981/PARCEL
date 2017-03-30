58c90f00685c50072953d4eab4182eb34281f05e
SHEN Yang <sheny@n068.default.domain>
2017-03-30 17:13:00 +0800

    parseCutAptLog.pl can handle multiple logs in one file

--- scripts/parseCutAptLog.pl
+++ scripts/parseCutAptLog.pl
@@ -15,26 +15,30 @@ if(@ARGV==0){
 }
 
 my $id = $ENV{"ID"} || "All";
-my $totalReads=0;
-my $writtenReads=0;
-my $adapterReads=0;
+my $addup = $ENV{"ADDUP"} || "yes";
+my $totalReadsAll=0;
+my $writtenReadsAll=0;
+my $adapterReadsAll=0;
 while(my $line=$fh->getline()){
 	if($line=~ m/Total reads processed:(.*)/){
-		$totalReads=$1;
+		my $totalReads=$1;
 		$totalReads=~ s/\s//sg;
 		$totalReads=~ s/,//sg;
+		$totalReadsAll = $addup eq "yes" ? $totalReads+$totalReadsAll : $totalReads;
 	}
 	if($line=~ m/Reads with adapters:(.*)\(/){
-		$adapterReads = $1;
+		my $adapterReads = $1;
 		$adapterReads =~ s/\s//sg;
 		$adapterReads =~ s/,//sg;
+		$adapterReadsAll = $addup eq "yes" ? $adapterReads + $adapterReadsAll : $adapterReads;
 	}
 	if($line=~ m/Reads written.*:(.*)\(/){
-		$writtenReads = $1;
+		my $writtenReads = $1;
 		$writtenReads =~ s/\s//sg;
 		$writtenReads =~ s/,//sg;
+		$writtenReadsAll = $addup eq "yes" ? $writtenReads + $writtenReadsAll : $writtenReads;
 	}
 }
-print $id."\t".$totalReads."\t".$writtenReads."\t".$adapterReads."\n";
+print $id."\t".$totalReadsAll."\t".$writtenReadsAll."\t".$adapterReadsAll."\n";
 
 
